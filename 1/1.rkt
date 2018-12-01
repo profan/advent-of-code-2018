4b32bd690352d52e182303f34ee5d447fb777ba9
Robin Hübner <profan@prfn.se>
2018-12-01 19:52:44 +0100

    day 1

---
+++ 1/1.rkt
@@ -0,0 +1,44 @@
+#lang racket
+
+(require racket/set)
+
+(define (calc-frequency input init-state)
+  (sequence-fold 
+    (lambda (acc-v e)
+      (define total (first acc-v))
+      (define fs (second acc-v))
+      (define ds (third acc-v))
+      (define new-freq 
+        (+ total 
+           (cond
+             [(non-empty-string? e)
+              (match (substring e 0 1)
+                ["+" (string->number (substring e 1))]
+                ["-" (- (string->number (substring e 1)))])])))
+      (define new-freq-set
+        (set-add fs new-freq))
+      (define new-freq-doubles
+        (cond 
+          [(and (set-member? fs new-freq) (not (member new-freq ds))) 
+           (append ds (list new-freq))]
+          [else 
+           ds]))
+      (list new-freq new-freq-set new-freq-doubles))
+    init-state input))
+
+(define (calc-until-has-duplicate lines [state (list 0 (set) '())])
+  (match-define (list cur-freq cur-freq-set cur-doubles) 
+    (calc-frequency lines state))
+  (cond
+    [(empty? cur-doubles)
+     (calc-until-has-duplicate lines (list cur-freq cur-freq-set cur-doubles))]
+    [else
+     (list cur-freq cur-freq-set cur-doubles)]))
+
+(match-define (list final-freq final-freq-set final-doubles)
+  (call-with-input-file "input.txt"
+                        (lambda (in)
+                          (define lines (port->lines in))
+                          (calc-until-has-duplicate lines))))
+
+(displayln (format "final freq: ~a~nfirst double: ~a" final-freq (first final-doubles)))
---
+++ README.md
@@ -0,0 +1,3 @@
+advent-of-code-2018
+-----------------------
+mostly in Racket probably!
